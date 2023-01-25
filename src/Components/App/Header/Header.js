f192fe87ad514057e812a04c55058f74da243a7f
Al Camara <Alhousseinec@gmail.com>
2023-01-24 21:57:51 -0600

    first commit

---
+++ src/Components/App/Header/Header.js
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+export function Header() {
+  return (
+    <header className="header-section">
+      <div className="header-hero">
+        <h1 className="hero-h1">
+          Responsive layouts don't have to be a struggle
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam.
+        </p>
+        <button className="btn">I want to Learn</button>
+      </div>
+    </header>
+  );
+}
